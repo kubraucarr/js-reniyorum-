2fdc1cda9f80edc6d87f61d12facf4b337cb36ad
Kubra Ucar <kvd.19998@gmail.com>
2024-03-11 22:27:54 +0300

    call aply ve bind

--- app.js
+++ app.js
@@ -435,3 +435,38 @@
 //console.log(emp);
 
 
+
+
+//CALL APPLY BİND
+
+const obj1={
+  number1:10,
+  number2:20
+};
+
+const obj2={
+  number1:30,
+  number2:40
+}
+
+function addNumbers(number3,number4){
+  console.log(this.number1 + this.number2 + number3 + number4);
+}
+//addNumbers(100,200); // burada this window objesi ile geliyordu
+
+//aşağıda this i obj1 ve obj2 ile çağıracağız
+//addNumbers.call(obj1,100,200);
+//addNumbers.call(obj2,100,200);
+
+//şimdi apply ile çağıralım 
+//addNumbers.apply(obj1,[100,200]);
+
+//şimdi bind i yapalım 
+function getNumbersTotal(number3,number4){
+  return this.number1 + this.number2 + number3 + number4;
+}
+
+const copyFunc1 = getNumbersTotal.bind(obj1);
+console.dir(copyFunc1);
+
+console.log(copyFunc1(100,200));
